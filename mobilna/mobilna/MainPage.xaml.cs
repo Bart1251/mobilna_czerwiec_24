08cc9600e74df237011234e0a7e9757907f2a56f
Bartłomiej Więcek <bartlomiej.wiecek@vp.pl>
2024-10-30 10:36:08 +0100

    Dodaj pliki projektów.

---
+++ mobilna/mobilna/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace mobilna
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            for(int i = 0; i < 5; i++)
+            {
+                Photos.Children.Add(new Image() { Source = "question.png", HeightRequest = 60 });
+            }
+        }
+    }
+}
